9ac6cfdae87d2c8d3e6457ed7477f4bba1ec0fa7
Ivan Koltcov <ikoltcov@yandex.ru>
2019-12-23 17:59:47 +0300

    master // Refactoring. Create struct and use it

--- main.cpp
+++ main.cpp
@@ -7,6 +7,11 @@
 #include <CL/opencl.h>
 #endif
 
+typedef struct {
+    int Id;
+    char* Name;
+} Parameter;
+
 int main(int argc, char** argv){
     cl_uint platformCount;
     cl_platform_id* platforms;
@@ -15,53 +20,74 @@ int main(int argc, char** argv){
     platforms = new cl_platform_id[platformCount];
     clGetPlatformIDs(platformCount, platforms, NULL);
 
-    char* value;
+    char* charValue;
     size_t valueSize;
-    cl_uint deviceCount;
-    cl_device_id* devices;
-    cl_uint maxComputeUnits;
+
+    cl_uint intValue;
+    cl_bool boolValue;
+
+    Parameter platformParameters[5] = {
+        { CL_PLATFORM_NAME, (char*)"CL_PLATFORM_NAME" },
+        { CL_PLATFORM_VENDOR, (char*)"CL_PLATFORM_VENDOR" },
+        { CL_PLATFORM_VERSION, (char*)"CL_PLATFORM_VERSION" },
+        { CL_PLATFORM_PROFILE, (char*)"CL_PLATFORM_PROFILE" },
+        { CL_PLATFORM_EXTENSIONS, (char*)"CL_PLATFORM_EXTENSIONS" }
+    }; 
+
+    Parameter deviceCharParameters[] = {
+        { CL_DEVICE_NAME, (char*)"CL_DEVICE_NAME" },
+        { CL_DEVICE_VERSION, (char*)"CL_DEVICE_VERSION" },
+        { CL_DEVICE_OPENCL_C_VERSION, (char*)"CL_DEVICE_OPENCL_C_VERSION" },
+        { CL_DRIVER_VERSION, (char*)"CL_DRIVER_VERSION" }
+    };
+
+    Parameter deviceBoolParameters[] = {
+        { CL_DEVICE_AVAILABLE, (char*)"CL_DEVICE_AVAILABLE" },
+        { CL_DEVICE_COMPILER_AVAILABLE, (char*)"CL_DEVICE_COMPILER_AVAILABLE" },
+    };
+
+    Parameter deviceNumberParameters[] = {
+        { CL_DEVICE_MAX_COMPUTE_UNITS, (char*)"CL_DEVICE_MAX_COMPUTE_UNITS" },
+        { CL_DEVICE_GLOBAL_MEM_SIZE, (char*)"CL_DEVICE_GLOBAL_MEM_SIZE" },
+        { CL_DEVICE_LOCAL_MEM_SIZE, (char*)"CL_DEVICE_LOCAL_MEM_SIZE" },
+        { CL_DEVICE_LOCAL_MEM_TYPE, (char*)"CL_DEVICE_LOCAL_MEM_TYPE" },
+        { CL_DEVICE_MAX_WORK_GROUP_SIZE, (char*)"CL_DEVICE_MAX_WORK_GROUP_SIZE" },
+        { CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS, (char*)"CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS" },
+        { CL_DEVICE_MAX_WORK_ITEM_SIZES, (char*)"CL_DEVICE_MAX_WORK_ITEM_SIZES" },
+    };
 
     for (int i = 0; i < platformCount; i++) {
+        
+        for(int j = 0; j < sizeof(platformParameters)/sizeof(*platformParameters); j++){
+            clGetPlatformInfo(platforms[i], platformParameters[j].Id, 0, NULL, &valueSize);
+            charValue = new char[valueSize];
+            clGetPlatformInfo(platforms[i], platformParameters[j].Id, valueSize, charValue, NULL);
+            printf("%s: %s\n", platformParameters[j].Name, charValue );
+            free(charValue);
+        }
+
+        cl_uint deviceCount;
+        cl_device_id* devices;
         clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, NULL, &deviceCount);
         devices = new cl_device_id[deviceCount];
         clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, deviceCount, devices, NULL);
 
-        for (int j = 0; j < deviceCount; j++) {
-            int k = 0;
-
-            clGetDeviceInfo(devices[j], CL_DEVICE_NAME, 0, NULL, &valueSize);
-            value = new char[valueSize];
-            clGetDeviceInfo(devices[j], CL_DEVICE_NAME, valueSize, value, NULL);
-            printf("%d. Device: %s\n", j+1, value);
-            free(value);
-
-            clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, 0, NULL, &valueSize);
-            value = new char[valueSize];
-            clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, valueSize, value, NULL);
-            printf(" %d.%d Hardware version: %s\n", j+1, ++k, value);
-            free(value);
-
-            clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, 0, NULL, &valueSize);
-            value = new char[valueSize];
-            clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, valueSize, value, NULL);
-            printf(" %d.%d Software version: %s\n", j+1, ++k, value);
-            free(value);
-
-            clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, 0, NULL, &valueSize);
-            value = new char[valueSize];
-            clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, valueSize, value, NULL);
-            printf(" %d.%d OpenCL C version: %s\n", j+1, ++k, value);
-            free(value);
-
-            clGetDeviceInfo(devices[j], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(maxComputeUnits), &maxComputeUnits, NULL);
-            printf(" %d.%d Parallel compute units: %d\n", j+1, ++k, maxComputeUnits);
-
-            clGetDeviceInfo(devices[j], CL_DEVICE_EXTENSIONS, 0, NULL, &valueSize);
-            value = new char[valueSize];
-            clGetDeviceInfo(devices[j], CL_DEVICE_EXTENSIONS, valueSize, value, NULL);
-            printf(" %d.%d Device extensions: %s\n", j+1, ++k, value);
-            
-            free(value);
+        for(int j = 0; j < deviceCount; j++){
+            for(int k = 0; k < sizeof(deviceCharParameters)/sizeof(*deviceCharParameters); k++){
+                clGetDeviceInfo(devices[j], deviceCharParameters[k].Id, 0, NULL, &valueSize);
+                charValue = new char[valueSize];
+                clGetDeviceInfo(devices[j], deviceCharParameters[k].Id, valueSize, charValue, NULL);
+                printf(k != 0 ? "\t\t%s: %s\n" : "\t%s: %s\n", + deviceCharParameters[k].Name, charValue);
+                free(charValue);    
+            }
+            for(int k = 0; k < sizeof(deviceNumberParameters)/sizeof(*deviceNumberParameters); k++){
+                clGetDeviceInfo(devices[j], deviceNumberParameters[k].Id, sizeof(intValue), &intValue, NULL);
+                printf("\t\t%s: %d\n", deviceNumberParameters[k].Name, intValue);
+            }
+            for(int k = 0; k < sizeof(deviceBoolParameters)/sizeof(*deviceBoolParameters); k++){
+                clGetDeviceInfo(devices[j], deviceBoolParameters[k].Id, sizeof(boolValue), &boolValue, NULL);
+                printf("\t\t%s: %s\n", deviceBoolParameters[k].Name, boolValue ? (char*)"True" : (char*)"False");
+            }      
         }
 
         free(devices);
